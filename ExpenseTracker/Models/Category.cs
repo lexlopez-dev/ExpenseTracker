2a68c655373d076a4268db37bff7cabdcaf80559
Alejandro <alopez@nurturethechildren.onmicrosoft.com>
2023-12-16 22:44:44 -0700

    Model classes

---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
+
+		[Column(TypeName = "nvarchar(50)")]
+		public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Icon { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string MyProperty { get; set; }
+	}
+}
+
