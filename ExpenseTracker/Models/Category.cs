da8dd1307237abbf8bb7f17c704fa29af67d8d0d
Alejandro <alopez@nurturethechildren.onmicrosoft.com>
2023-12-17 21:55:11 -0700

    Migrated to Azure SQL server DB succesfully

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -12,11 +12,11 @@ namespace ExpenseTracker.Models
 		[Column(TypeName = "nvarchar(50)")]
 		public string Title { get; set; }
 
-        [Column(TypeName = "nvarchar(50)")]
-        public string Icon { get; set; }
+		[Column(TypeName = "nvarchar(5)")]
+		public string Icon { get; set; } = "";
 
-        [Column(TypeName = "nvarchar(50)")]
-        public string MyProperty { get; set; }
+		[Column(TypeName = "nvarchar(10)")]
+		public string Type { get; set; } = "Expense";
 	}
 }
 
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Dependency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
